a59795e1149207c9e3853674e4c734a4107fdb77
CaptSiro <honzahorak433@icloud.com>
2023-09-07 17:30:47 +0200

    Moved variable assignment (yes that fixes a bug)

--- src/kmean.php
+++ src/kmean.php
@@ -12,21 +12,22 @@ require_once __DIR__ . "/point-distance.php";
 
 
 /**
+ * @return Centroid[]
  * @throws Exception
  */
-function kmean(array $points, array $centroids, int $point_dimensions, float $threshold = 0.001): array {
-    $dist = 0;
+function kmean(array $points, array $centroids, int $point_dimensions, float $threshold = 0.01): array {
     $centroid_count = count($centroids);
 
     do {
+        $dist = 0;
         $moved = move_centroids($points, $centroids, $point_dimensions);
 
         for ($i = 0; $i < $centroid_count; $i++) {
-            $dist += point_distance($moved[$i], $centroids[$i], $point_dimensions);
+            $dist += point_distance($centroids[$i], $moved[$i]->point, $point_dimensions);
         }
 
         $centroids = $moved;
-    } while ($dist / $centroid_count < $threshold);
+    } while ($dist / $centroid_count > $threshold);
 
     return $centroids;
 }

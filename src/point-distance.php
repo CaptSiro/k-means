6ef0a15875838e3aca8ef7a296e1bcaa87f877ca
CaptSiro <honzahorak433@icloud.com>
2023-09-07 17:28:34 +0200

    Using function pow instead of syntax ^ (2)

--- src/point-distance.php
+++ src/point-distance.php
@@ -4,11 +4,17 @@ namespace KMean;
 
 
 
-function point_distance(array $from, array $to, int $size): float {
+function point_distance(Centroid|array $from, array $to, int $size): float {
     $sum = 0;
 
-    for ($i = 0; $i < $size; $i++) {
-        $sum += ($from[$i] - $to[$i]) ^ 2;
+    if ($from instanceof Centroid) {
+        for ($i = 0; $i < $size; $i++) {
+            $sum += pow($from->point[$i] - $to[$i], 2);
+        }
+    } else {
+        for ($i = 0; $i < $size; $i++) {
+            $sum += pow($from[$i] - $to[$i], 2);
+        }
     }
 
     return sqrt($sum);
